33dca9cad257f0213879146308efef796cb3a41c
asimhasan <asim.hasan@duke.edu>
2015-11-29 00:35:21 +0000

    Still trying

--- feedme/settings.py
+++ feedme/settings.py
@@ -106,7 +106,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = '/feeder/static/'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
