da8281468c62b14a9abb9244ae8b8fee95c17c9f
asimhasan <asim.hasan@duke.edu>
2015-11-29 00:39:35 +0000

    Still trying

--- feedme/settings.py
+++ feedme/settings.py
@@ -111,7 +111,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR+'feeder/, 'static'),
 )
 
 # Parse database configuration from $DATABASE_URL
