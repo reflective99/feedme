f0db411558d70575f33e7f809e12258af1f48fa6
asimhasan <asim.hasan@duke.edu>
2015-11-29 01:21:49 +0000

    Maybe deployed?

--- feedme/wsgi.py
+++ feedme/wsgi.py
@@ -8,4 +8,4 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
