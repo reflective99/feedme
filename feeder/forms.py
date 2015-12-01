ca919e42184744b202d6d2fe69ef53b7345344a3
asimhasan <asim.hasan@duke.edu>
2015-12-01 01:12:00 +0000

    Fixed Form Styling

--- feeder/forms.py
+++ feeder/forms.py
@@ -2,10 +2,16 @@
 from django.contrib.auth.models import User
 from django import forms
 
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        
+    def __init__(self, *args, **kwargs):
+            super(UserForm, self).__init__(*args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
 
