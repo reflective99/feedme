3e4336c8dcf58cfa5f6ba110eadd26e45dc1a080
asimhasan <asim.hasan@duke.edu>
2015-12-07 22:48:14 +0000

    DB reconfigured

--- feeder/admin.py
+++ feeder/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Restaurant
+from .models import Category
+from .models import Like
+
 
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'city', 'rating')
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('cat')
 
 admin.site.register(Restaurant, RestaurantAdmin)
-
+admin.site.register(Category, CategoryAdmin)
